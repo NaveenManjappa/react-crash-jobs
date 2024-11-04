ef016c37da50e24f1170bf32ba41a49677be614b
Naveen Manjappa <NManjappa@manh.com>
2024-11-04 11:39:06 +0000

    Trying JSX code

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,25 @@ const App = () => {
   const x = 10;
   const y = 20;
   const names = ['Brad','Mary','Joe'];
+  const loggedIn = true;
+
+  const styles = {
+    color:'red',
+    fontSize:'55px'
+  }
+
   return (
     <>
       <div className='text-6xl'>App</div>
-      <p>Hello { name }</p>
+      <p style={styles}>Hello { name }</p>
       <p>The sum of {x} and {y} is {x+y}</p>
       <ul>
-        {names.map((el) => (
-          <li>{el}</li>
+        {names.map((el,index) => (
+          <li key={index}>{el}</li>
         ))}
       </ul>
+
+        { loggedIn && <h1>Hello Member</h1>}
     </>
   );
 };
