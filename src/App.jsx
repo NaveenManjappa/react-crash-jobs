4d1d71b99b14c9ec0eaf56377b84bd68b8577fa6
Naveen Manjappa <NManjappa@manh.com>
2024-11-11 16:35:02 +0000

    Delete job with toast message

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,33 @@ import AddJobPage from './pages/AddJobPage';
 
 
 const App = () => {
-  const addJob = (newJob) => {
-    console.log('newJob',newJob);
+  //ADD new job
+  const addJob = async (newJob) => {
+    const res = await fetch('/api/jobs',{
+      method: 'POST',
+      headers: {
+        'Content-Type':'application/json'
+      },
+      body: JSON.stringify(newJob)
+    });
+    return;
   };
-  
+
+  //DELETE jobs
+  const deleteJob = async (id) => {
+    const res = await fetch(`/api/jobs/${id}`,{
+      method: 'DELETE'     
+    });
+    return;
+  }
+
   const router = createBrowserRouter(
     createRoutesFromElements(
     <Route path='/' element={<MainLayout/>}>
       <Route index element={<HomePage />}/>
       <Route path='/jobs' element={<JobsPage/>}/>
       <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob}/>}/>
-      <Route path='/jobs/:id' element={<JobPage/>} loader={jobLoader}/>
+      <Route path='/jobs/:id' element={<JobPage deleteJob = {deleteJob}/>} loader={jobLoader}/>
       <Route path='*' element={<NotFoundPage/>}/>
     </Route>
     )
